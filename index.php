fdf851995c103afd31c527301cf02c389d1f40af
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-07 19:30:45 +0200

    [UPD]Added function in function.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-    function passwordGen(int $length){
-        $chars=array_merge(range(0, 9), range('A', 'z'));
-        $password='';
-        for($i=0; $i < $length; $i++){
-            $password.=$chars[mt_rand(0, count($chars)-1)];
-        }
-        return $password;
-    }
+    include './function.php';
 
     if(isset($_GET['numbergen']) && $_GET['numbergen'] !==''){
         $yourPassword=passwordGen($_GET['numbergen']);
