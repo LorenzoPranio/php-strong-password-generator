73c3b443080644d0dc7dacd3e2cfe79b61e35a3d
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-07 19:08:30 +0200

    [UPD]Added function in index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
+    function passwordGen(int $length){
+        $chars=array_merge(range(0, 9), range('A', 'z'));
+        $password='';
+        for($i=0; $i < $length; $i++){
+            $password.=$chars[mt_rand(0, count($chars)-1)];
+        }
+        return $password;
+    }
 ?>
 
 <!DOCTYPE html>
